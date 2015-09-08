c278aafd44280461128b6bcfd02d98792cc0e6a3
sandspider2234 <bnehmad@gmail.com>
2015-09-08 15:56:21 +0300

    Extracted proc splitting in enumRequiredRegisters.py into function.

--- enumRequiredRegisters.py
+++ enumRequiredRegisters.py
@@ -1,12 +1,9 @@
-from asmlib import consts
+from AsmLib import consts
 from argparse import ArgumentParser
 
 
 def create_reg_list(proc_string):
-    new_list = []
-    for register in consts.REGISTERS_LIST:
-        if register in proc_string:
-            new_list.append(register)
+    new_list = [register for register in consts.REGISTERS_LIST if register in proc_string]
 
     for register in consts.SMALL_REGISTERS_LIST:
         if register in proc_string:
@@ -21,41 +18,38 @@ def create_reg_list(proc_string):
     return new_list
 
 
-def main():
-    d = {}
-    parser = ArgumentParser()
-    parser.add_argument("-filename", default="source.asm",
-                        help="File to be enumerated. If file is in current folder, filename only is OK. "
-                             "Else, full path is required.", metavar="path")
-    file_string = open(parser.parse_args().filename, "r").read()
-    # CR Shay   Extract a function which gets the file's contents (AKA file_string) and returns a list of all the procs
-    #           like currentProc.
-    #           Then you could write something like this:
-    """
-    for proc in get_all_procs(file_content):
-        do_something_with_proc(proc)
-    """
+def get_all_procs(file_string):
+    proc_list = []
     begin_index = file_string.index("proc")
     final_index = file_string.rindex("endp")
     proc_seg_slice = file_string[begin_index:final_index + 4]
     beginning_of_proc = proc_seg_slice.index("proc")
     ending_of_proc = proc_seg_slice.index("endp")
     while ending_of_proc <= final_index:
-        current_proc = proc_seg_slice[beginning_of_proc:ending_of_proc]
-        current_registers_list = create_reg_list(current_proc)
-        proc_name = current_proc.rsplit(None, 1)[-1]
-        d[proc_name] = current_registers_list[:]
-        # CR Shay   The bug is caused by the code's flawed design, and is finally exposed when this line runs.
-        #           Figuring out what's wrong and how to change the code to fix it is left to you as an assignment.
-        #           You didn't expect it to be that easy, now did you? :)
-        current_registers_list.clear()
+        proc_list.append(proc_seg_slice[beginning_of_proc:ending_of_proc])
         proc_seg_slice = proc_seg_slice[ending_of_proc + 4:final_index]
         beginning_of_proc = proc_seg_slice.find("proc")
         ending_of_proc = proc_seg_slice.find("endp")
         if ending_of_proc == -1 or beginning_of_proc == -1:
             break
+    return proc_list
 
-    print(d)
+
+def main():
+    proc_dict = {}
+    parser = ArgumentParser()
+    parser.add_argument("-filename", default="source.asm",
+                        help="File to be enumerated. If file is in current folder, filename only is OK. "
+                             "Else, full path is required.", metavar="path")
+    file_string = open(parser.parse_args().filename, "r").read()
+
+    for proc in get_all_procs(file_string):
+        current_registers_list = create_reg_list(proc)
+        proc_name = proc.rsplit(None, 1)[-1]
+        proc_dict[proc_name] = current_registers_list[:]
+        current_registers_list.clear()
+
+    print(proc_dict)
 
 
 if __name__ == "__main__":
